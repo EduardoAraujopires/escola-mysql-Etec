d677cf9f905bcfa744d2b456a9905022c188e054
Eduardo Araújo <eduaraujopires370@gmail.com>
2025-08-27 22:10:59 -0300

    Atualição

--- src/main/java/com/etec/escola/Service/Professor_Service.java
+++ src/main/java/com/etec/escola/Service/Professor_Service.java
@@ -1,27 +1,4 @@
 package com.etec.escola.Service;
 
-import com.etec.escola.Interface.Professor_Repository;
-import com.etec.escola.Models.Professor;
-
-import java.util.List;
-
 public class Professor_Service {
-
-    private final Professor_Repository professorRepository;
-
-    public Professor_Service( Professor_Repository professorRepository){
-        this.professorRepository = professorRepository;
-    }
-
-    public List<Professor> buscarProfessor(){
-        return professorRepository.findAll();
-    }
-
-    public Professor salvarProfessor(Professor professor){
-        return  professorRepository.save(professor);
-    }
-
-    public Professor buscarId(Long id){
-        return professorRepository.findById(id).orElse(null);
-    }
 }
