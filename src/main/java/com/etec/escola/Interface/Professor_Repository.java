634f0046b4f3a8d62b0f992c29e3dec9c2d11edc
Eduardo Araújo <eduaraujopires370@gmail.com>
2025-09-03 16:50:58 -0300

    ..

--- src/main/java/com/etec/escola/Interface/Professor_Repository.java
+++ src/main/java/com/etec/escola/Interface/Professor_Repository.java
@@ -1,7 +1,4 @@
 package com.etec.escola.Interface;
 
-import com.etec.escola.Models.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Professor_Repository extends JpaRepository <Professor, Long> {
+public interface Professor_Repository {
 }
