8e82ead3eb16289f11b9e0810a0c61e6db1476ce
Eduardo Araújo <eduaraujopires370@gmail.com>
2025-09-24 17:21:56 -0300

    .

--- src/main/resources/static/professor.js
+++ src/main/resources/static/professor.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('http://localhost:8080/aluno')
+  fetch('http://localhost:8080/professor')
     .then(response => response.json())
     .then(data => {
-      const tbody = document.getElementById('aluno-tbody');
-      data.forEach(aluno => {
+      const tbody = document.getElementById('professor-tbody');
+      data.forEach(professor => {
         const tr = document.createElement('tr');
 
         tr.innerHTML = `
-          <td>${aluno.nome}</td>
-          <td>${aluno.email}</td>
-          <td>${aluno.telefone}</td>
+          <td>${professor.nome}</td>
+          <td>${professor.email}</td>
+          <td>${professor.telefone}</td>
         `;
 
         tbody.appendChild(tr);
       });
     })
     .catch(error => {
-      console.error('Erro ao buscar alunos:', error);
-      alert('Erro ao carregar a lista de alunos.');
+      console.error('Erro ao buscar professors:', error);
+      alert('Erro ao carregar a lista de professors.');
     });
 });
